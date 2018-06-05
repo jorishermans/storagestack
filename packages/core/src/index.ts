e09d55914500818090d07242252b1815fe8f2ea1
jorishermans <jhermans85@hotmail.com>
2018-06-05 08:17:51 +0200

    Improve building workflow

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,7 @@
 import { Application } from "./application";
+export { Provider } from './provider';
+export { MemoryProvider } from './memory.provider';
+export { MiddlewareStack } from './middleware-stack';
+export { StorageInfo, BasicInfo } from "./middleware-holder";
 
 export const ss = new Application();
