e75ed092cab9a638a8180612dfc655f52102b813
jorishermans <jhermans85@hotmail.com>
2018-07-12 23:36:31 +0200

    finetune package configuration for sub packages

--- examples/basic/src/index.ts
+++ examples/basic/src/index.ts
@@ -1,7 +1,8 @@
-import { ss, MemoryProvider } from '@storagestack/core';
+import { ss } from '@storagestack/core';
 import { JsonMiddleware } from '@storagestack/json-middleware';
+import { LocalStorageProvider } from '@storagestack/localstorage-provider';
 
-ss.registerProvider(new MemoryProvider());
+ss.registerProvider(new LocalStorageProvider());
 
 ss.use('*', new JsonMiddleware());
 ss.set('cool', 'asset');
