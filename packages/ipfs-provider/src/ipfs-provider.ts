b4d125fc0fb6a2d36d92c0aafb3af1f288f39ddf
jorishermans <jhermans85@hotmail.com>
2018-07-21 00:00:35 +0200

    adapting the code of ipfs so it works with all names

--- packages/ipfs-provider/src/ipfs-provider.ts
+++ packages/ipfs-provider/src/ipfs-provider.ts
@@ -6,12 +6,16 @@ export class IpfsProvider<T> implements Provider<T> {
     constructor(private ipfs) { }
 
     set(name: string, content: T, options?: Object): Promise<void> {
+        if (!name.startsWith('/')) name = `/${name}`;
+        console.log(name);
         return this.ipfs.files.write(name, Buffer.from(content), options ? options : { offset: 0, create: true }, (err) => {
-            return Promise.reject(err);
+            return err? Promise.reject(err) : Promise.resolve();
         });
     }
 
     get(name: string, options?: Object): Promise<T> {
+        if (!name.startsWith('/')) name = `/${name}`;
+        console.log(name);
         return this.ipfs.files.read(name, options ? options : { offset: 0 }, (err, buf) => {
             if (err) {
                 return Promise.reject(err);
@@ -22,6 +26,7 @@ export class IpfsProvider<T> implements Provider<T> {
     }
 
     delete(name: string, options?: Object): Promise<void> {
+        if (!name.startsWith('/')) name = `/${name}`;
         return this.ipfs.files.rm(name, options ? options : { recursive: true }, (err) => {
             return Promise.reject(err);
         });
