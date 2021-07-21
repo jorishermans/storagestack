01a8bacc7894fee55f6be038da95f39acbb64be8
joris hermans <JHermans85@hotmail.com>
2021-07-21 11:09:25 +0200

    test search middleware with json middleware

---
+++ packages/search-middleware/tests/json-search-with-middleware.spec.ts
@@ -0,0 +1,41 @@
+
+import { ss, MemoryProvider, MiddlewareStack, StorageInfo } from '@storagestack/core';
+import { SearchMiddleware } from '../src/search-middleware';
+
+export class JsonMiddleware implements MiddlewareStack {
+
+    set(storageInfo: StorageInfo, next: () => void) {
+        storageInfo.content = JSON.stringify(storageInfo.content);
+        next();
+    }
+    
+    get(storageInfo: StorageInfo, next: () => void) {
+        try {
+            if (typeof storageInfo.content === 'string') {
+                storageInfo.content = storageInfo.content !== '' ? JSON.parse(storageInfo.content) : undefined;
+            }
+            next();
+        } catch { console.warn('JSON middleware parse error ... ', storageInfo.name, storageInfo.origin); }
+    }
+}
+
+describe('Search Middleware with json', () => {
+    test('should perform a basic search and can retrieve the json documents', async () => {
+        let memory = {};
+        
+        ss.registerProvider(new MemoryProvider(memory));
+        const searchMiddleware = new SearchMiddleware();
+        ss.use('**', new JsonMiddleware());
+        ss.use('/files/*', searchMiddleware);
+        await ss.set('/files/text', {some: "Cool!", date: new Date(), count: 5, text: 'Help me out.'});
+        await ss.set('/files/cool.txt', {some: "Cool!", date: new Date(), count: 5, text: 'Help me out. Or maybe not!'});
+        await ss.set('/files/java.txt', {some: "java!", date: new Date(), count: 5, text: 'This is not what I want'});
+        await ss.set('/files/oyd.txt', {some: "oyd!", date: new Date(), count: 5, text: 'own your own data, is the future for full privacy.'});
+        await ss.set('/files/data-types.txt', 'In computer science and computer programming, a data type or simply type is a classification identifying one of various types of data, such as real, integer or Boolean, that determines the possible values for that type; the operations that can be done on values of that type; the meaning of the data; and the way values of that type can be stored.');
+
+        const results = await searchMiddleware.search('Cool! Help me out.');
+        expect(results).toHaveLength(2);
+        expect(results[0].name).toBe("/files/text");
+    });
+
+});
